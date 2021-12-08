2fcd02f1ded1b0cd897fab88d8e983e69ec7c8bf
植薰 <zhixundeng@gmail.com>
2021-12-08 19:18:15 +0000

    [+] Launch screen time -> 3.0 second

--- HandyBand/UI_Design/HandyBandApp.swift
+++ HandyBand/UI_Design/HandyBandApp.swift
@@ -5,11 +5,23 @@
 //  Created by Dan on 10/28/21.
 // Who is this file?
     // The Master View
+    // App Delegate
+    // Set Launch Screen time
 
 import SwiftUI
 
+// Create UIApplication Delegate
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        Thread.sleep(forTimeInterval: 3.0)
+        return true
+    }
+}
+
 @main
 struct HandyBandApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var fileManage = FileManageLogic()
     var recorderPlayer = RecordingLogic()
     var playCreationModel = playCreationsModel()
